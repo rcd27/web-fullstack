8d892c4a6de35c5d69b82e6260b854138650cc9d
s.zemlyakov <redtom@yandex.ru>
2024-04-29 17:30:14 +0300

    feat: add Promise chain

--- useContext/src/App.jsx
+++ useContext/src/App.jsx
@@ -4,17 +4,17 @@ const ThemeContext = createContext(null);
 
 function Form() {
     return (
-            <Panel title="Welcome">
-                <Button>Sign up</Button>
-                <Button>Sign in</Button>
-            </Panel>
+        <Panel title="Welcome">
+            <Button>Sign up</Button>
+            <Button>Sign in</Button>
+        </Panel>
     )
 }
 
 function Panel({title, children}) {
     const theme = useContext(ThemeContext);
     const className = 'panel' + theme;
-    return(
+    return (
         <section className={className}>
             <h1>{title}</h1>
             {children}
@@ -25,7 +25,7 @@ function Panel({title, children}) {
 function Button({children}) {
     const theme = useContext(ThemeContext);
     const className = 'button' + theme;
-    return(
+    return (
         <button className={className}>
             {children}
         </button>
@@ -50,5 +50,33 @@ export default function App() {
         </ThemeContext.Provider>
     );
 }
+
+// Промисы и цепочки
+let promise1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('First')
+    }, 1000)
+})
+
+let promise2 = (result) => {
+    return new Promise((resolve, reject) => {
+        resolve(result + ', second')
+    })
+}
+
+let promise3 = (result) => {
+    return new Promise((resolve, reject) => {
+        reject(reject + 'fck off')
+    })
+}
+
+promise1.then(promise2)
+    .then(promise3)
+    .then((result) => {
+        console.log(result)
+    })
+    .catch((error) => {
+        console.log(error)
+    })
 
 
